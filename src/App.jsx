a953a223152a4882044493975ba98f73f3a33343
aminul-islam01 <abiddut953@gmail.com>
2023-05-18 19:29:42 +0600

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { Outlet } from 'react-router-dom'
 import './App.css'
+import Navbar from './Pages/Shared/Navbar/Navbar'
 
 function App() {
 
   return (
     <>
-      <h2>header</h2>
+      <Navbar></Navbar>
       <Outlet></Outlet>
       <h2>footer</h2>
     </>
