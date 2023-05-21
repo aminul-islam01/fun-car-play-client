2b9e82bea1ea9e9fa1e6341840c9a2338b68c7c2
aminul-islam01 <abiddut953@gmail.com>
2023-05-21 23:54:22 +0600

    edit footer

--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -1,40 +1,47 @@
+import { Link } from "react-router-dom";
+import { FaEye, FaEyeSlash, FaFacebook, FaGoogle, FaInstagram, FaTwitter } from 'react-icons/fa';
 
 const Footer = () => {
     return (
-        <footer className="footer p-10 bg-base-200 text-base-content">
-            <div>
-                <span className="footer-title">Services</span>
-                <a className="link link-hover">Branding</a>
-                <a className="link link-hover">Design</a>
-                <a className="link link-hover">Marketing</a>
-                <a className="link link-hover">Advertisement</a>
-            </div>
-            <div>
-                <span className="footer-title">Company</span>
-                <a className="link link-hover">About us</a>
-                <a className="link link-hover">Contact</a>
-                <a className="link link-hover">Jobs</a>
-                <a className="link link-hover">Press kit</a>
-            </div>
-            <div>
-                <span className="footer-title">Legal</span>
-                <a className="link link-hover">Terms of use</a>
-                <a className="link link-hover">Privacy policy</a>
-                <a className="link link-hover">Cookie policy</a>
-            </div>
-            <div>
-                <span className="footer-title">Newsletter</span>
-                <div className="form-control w-80">
-                    <label className="label">
-                        <span className="label-text">Enter your email address</span>
-                    </label>
-                    <div className="relative">
-                        <input type="text" placeholder="username@site.com" className="input input-bordered w-full pr-16" />
-                        <button className="btn btn-primary absolute top-0 right-0 rounded-l-none">Subscribe</button>
+        <div className="bg-base-200  p-10">
+            <Link to="/" className="text-xl font-semibold"><span className="text-blue-700 font-bold font-serif">FunCar</span> Play</Link>
+            <footer className="footer text-base-content">
+                <div>
+                    <span className="footer-title">Services</span>
+                    <a className="link link-hover">Branding</a>
+                    <a className="link link-hover">Design</a>
+                    <a className="link link-hover">Marketing</a>
+                    <a className="link link-hover">Advertisement</a>
+                </div>
+                <div>
+                    <span className="footer-title">Address</span>
+                    <a className="link link-hover">Dhaka Uttara Bangladesh</a>
+                    <a className="link link-hover">Dhaka Mirpur-1 Bangladesh</a>
+                </div>
+                <div>
+                    <span className="footer-title">Contact us</span>
+                    <a className="">sunday to thursday 9am to 8pm</a>
+                    <a className="link link-hover">+88 0123456789</a>
+                    <div className="flex gap-3">
+                        <FaFacebook className="text-2xl cursor-pointer text-blue-700"></FaFacebook>
+                        <FaTwitter className="text-2xl cursor-pointer text-blue-700"></FaTwitter>
+                        <FaInstagram className="text-2xl cursor-pointer text-blue-700"></FaInstagram>
+                    </div> 
+                </div>
+                <div>
+                    <span className="footer-title">Newsletter</span>
+                    <div className="form-control w-80">
+                        <label className="label">
+                            <span className="label-text">Enter your email address</span>
+                        </label>
+                        <div className="relative">
+                            <input type="text" placeholder="username@site.com" className="input input-bordered w-full pr-16" />
+                            <button className="btn btn-primary absolute top-0 right-0 rounded-l-none">Subscribe</button>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </footer>
+            </footer>
+        </div>
     );
 };
 
